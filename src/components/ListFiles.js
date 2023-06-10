33e90bb462d538ec313a3854c835d0bb426a53d2
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-06-10 22:38:17 +0300

    Finish All

--- src/components/ListFiles.js
+++ src/components/ListFiles.js
@@ -2,23 +2,20 @@ import React from 'react';
 import Image from './Image';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { db, doc, getDoc } from '../firebaseConfig';
 
 const ListFiles = () => {
   const dataArr = useSelector((state) => state.categoryData);
-  //const allRef = doc(db, "Data", "allData");
-  //const allSnap = await getDoc(allRef);
-  //const allItem = allSnap.data();
-  //const storedArray = dataArr.allImgs;
-  console.log(dataArr);
+  const storedArray = dataArr.allImgs;
+  const data = dataArr.allData;
   return (
     <div className='main-container'>
-      {/*{data.map((value, key) => {
+      {data.map((value, key) => {
+        console.log(value.name);
         return (
           <div key={key}>
-            <h2 className='category-title'>{value[0]}</h2>
+            <h2 className='category-title'>{value.name}</h2>
             <div className='category-container'>
-              {value[1].map((img, item) => {
+              {value.imgs.map((img, item) => {
                 return (
                   <Link
                     className='image-container'
@@ -33,7 +30,7 @@ const ListFiles = () => {
             </div>
           </div>
         )
-      })}*/}
+      })}
     </div >
   );
 }
